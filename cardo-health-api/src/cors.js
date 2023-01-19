d118c2c976ceda617bb7dd3b44fcb262be5b9975
rafaguerrero <rafa.guerrero@shellock.co>
2023-01-19 09:44:53 +0100

    Remove cors

--- cardo-health-api/src/cors.js
+++ cardo-health-api/src/cors.js
@@ -1,15 +1,9 @@
 const cors = require('cors');
 
-const WHITELIST_ORIGINS = ['http://localhost:3001', 'http://localhost:3000'];
-
 const corsOptions = {
     credentials: true,
     origin: function (origin, callback) {
-        if (WHITELIST_ORIGINS.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS - ' + origin));
-        }
+        callback(null, true);
     }
 };
 
